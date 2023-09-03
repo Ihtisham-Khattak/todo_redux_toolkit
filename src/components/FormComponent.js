1baaf8ef589792c6a9bb8922bfab3b411d1f29a4
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-09-04 01:42:44 +0500

    form changes

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,39 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const FormComponent = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   email: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // Handle form submission here, e.g., send data to a server or perform some action
+  //   console.log(formData);
+  // };
+
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    zipCode: "",
   });
+  console.log("🚀 ~ file: FormComponent.js:27 ~ FormComponent ~ form:", form);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value,
+    });
   };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here, e.g., send data to a server or perform some action
-    console.log(formData);
-  };
-
   return (
     <div className="container">
-      <h1>React Bootstrap Form</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className="text-light">React Bootstrap Form</h1>
+      <form className="text-light">
+        {/* First Name */}
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
-            Name
+            First Name
           </label>
           <input
             type="text"
             className="form-control"
-            id="name"
-            name="name"
-            value={formData.name}
+            id="firstName"
+            name="firstName"
+            // value={formData.name}
             onChange={handleChange}
           />
         </div>
+        {/* Last Name */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            // value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Email */}
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
             Email
@@ -43,7 +78,35 @@ const FormComponent = () => {
             className="form-control"
             id="email"
             name="email"
-            value={formData.email}
+            // value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Address */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="address"
+            name="address"
+            // value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Zip Code */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Zip Code
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="zipCode"
+            name="zipCode"
+            // value={formData.name}
             onChange={handleChange}
           />
         </div>
