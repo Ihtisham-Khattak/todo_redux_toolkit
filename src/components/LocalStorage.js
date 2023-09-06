0455e8963508102b1a0c0ea578d336cce91db754
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-09-06 22:45:45 +0500

    local storage chagnes

--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -1,21 +1,30 @@
 import React from "react";
 
 const LocalStorage = () => {
-
   // Accessing the localstorage
   console.log(window.localStorage);
   //Set the item in the local storage
-  window.localStorage.setItem('first', 'values')
-  console.log('LocalStorage Vlaues \t' + localStorage.length)
+  window.localStorage.setItem("first", "values");
+  console.log("LocalStorage Vlaues \t" + localStorage.length);
   //Get the item in the local storage
-  const getItems = localStorage.getItem("first")
-  console.log("🚀 ~ file: LocalStorage.js:11 ~ LocalStorage ~ getItems:", getItems)
-  
-
-  return (
-  
-    <div>LocalStorage</div>
+  const getItems = localStorage.getItem("first");
+  console.log(
+    "🚀 ~ file: LocalStorage.js:11 ~ LocalStorage ~ getItems:",
+    getItems
   );
+
+  //Local on the local storage items
+  window.localStorage.setItem("Fruits", "Apple");
+  window.localStorage.setItem("Vegetables", "Onion");
+  window.localStorage.setItem("Fruits", "Banana");
+  window.localStorage.setItem("Vegetables", "Lady FInger");
+
+  const keys = Object.keys(localStorage)
+  for(let key of keys){
+    console.log("Key Value Pair" + localStorage.setItem(key))
+  }
+
+  return <div>LocalStorage</div>;
 };
 
 export default LocalStorage;
